1f710189a0d5694937784e53191ed63114a72d77
Rafael Lang <rafael.lang9@gmail.com>
2023-10-31 01:14:31 -0400

    preflighted requests / configurando as permissoes do CORS

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,7 @@
 module.exports = (request, response, next) => {
   // Liberação de CORS por middleware
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // liberar para todos *
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
   next();
 };
